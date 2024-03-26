a578e50610af4ae36848c48669b4ae075fcc9b94
avinashraj-151 <avinashrajraj628@gmail.com>
2024-03-26 17:12:48 +0530

    install react router dom

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Nopage from "./Pages/Nopage/Nopage";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Nopage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
